caf06b865e08625ed6d65704c7771b3a46c64858
yumetia <yumetia000@gmail.com>
2026-03-16 12:27:22 -0400

    fix(Model User): updated at laravel forcing handling

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,10 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = 'Users';
+    public $timestamps = true;
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
 
     protected $fillable = [
         'username',
