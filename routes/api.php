23d49b4d2c2053855af00214ddac50ea8f1ad733
yumetia <yumetia000@gmail.com>
2026-03-18 11:43:35 -0400

    chore(api.php): clean routes

--- routes/api.php
+++ routes/api.php
@@ -1,38 +1,46 @@
 <?php
 
-use App\Http\Controllers\Controller; 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\User\UserController;
-
 use App\Http\Controllers\Post\PostController;
 use App\Http\Controllers\Like\LikeController;
+use App\Http\Controllers\Forum\ForumController;
 
-
+// Auth
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/posts', [PostController::class, 'index']);
-    Route::get('/posts/{id}', [PostController::class, 'show']);
 
-    
-    
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/likes/toggle', [LikeController::class, 'toggle']);
-
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/me', [AuthController::class, 'me']);
-        Route::patch('/profile', [UserController::class, 'updateProfile']);
-        Route::put('/disable/{id}',[UserController::class, 'disableAccount']);
-
-        Route::post('/posts', [PostController::class, 'store']);
-        Route::patch('/posts/{id}', [PostController::class, 'update']);
-        Route::delete('/posts/{id}', [PostController::class, 'destroy']);
     });
 });
 
+// Users
+Route::middleware('auth:sanctum')->prefix('users')->group(function () {
+    Route::patch('/profile', [UserController::class, 'updateProfile']);
+    Route::put('/disable/{id}', [UserController::class, 'disableAccount']);
+});
 
-use App\Http\Controllers\Forum\ForumController;
+// Posts
+Route::prefix('posts')->group(function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('/{id}', [PostController::class, 'show']);
 
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', [PostController::class, 'store']);
+        Route::patch('/{id}', [PostController::class, 'update']);
+        Route::delete('/{id}', [PostController::class, 'destroy']);
+    });
+});
+
+// Likes
+Route::middleware('auth:sanctum')->prefix('likes')->group(function () {
+    Route::post('/toggle', [LikeController::class, 'toggle']);
+});
+
+// Forum
 Route::prefix('forum')->group(function () {
     Route::get('/topics', [ForumController::class, 'index']);
     Route::get('/topics/{id}', [ForumController::class, 'show']);
