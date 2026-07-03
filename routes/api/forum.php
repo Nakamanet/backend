5a9c066805add2c88e11ca8b8150dc729660a0be
yumetia <yumetia000@gmail.com>
2026-07-03 17:03:20 +0200

    feat: preference Notification + refactor routes api splitted in several files

---
+++ routes/api/forum.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Forum\ForumController;
+
+Route::prefix('forum')->group(function () {
+    Route::get('/topics', [ForumController::class, 'index']);
+    Route::get('/topics/{id}', [ForumController::class, 'show']);
+
+    Route::middleware(['auth:api', 'user.active'])->group(function () {
+        Route::post('/topics', [ForumController::class, 'store']);
+        Route::delete('/topics/{id}', [ForumController::class, 'destroy']);
+        Route::post('/topics/{id}/reply', [ForumController::class, 'reply']);
+        Route::post('/topics/{id}/vote', [ForumController::class, 'voteOnTopic']);
+        Route::post('/replies/{id}/vote', [ForumController::class, 'voteOnReply']);
+    });
+});
