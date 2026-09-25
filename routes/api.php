e3d1c4edbea6f0de80b1a5f6cad5a835cd49c0c9
p4ris <stefan-paris.paduraru@epitech.eu>
2026-09-25 16:06:10 +0200

    chore(debug): add route to view logs

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/debug-logs', function () {
+    return response()->file(storage_path('logs/laravel.log'));
+});
+
 require __DIR__ . '/api/auth.php';
-require __DIR__ . '/api/users.php';
-require __DIR__ . '/api/posts.php';
-require __DIR__ . '/api/reports.php';
-require __DIR__ . '/api/likes.php';
+require __DIR__ . '/api/user.php';
+require __DIR__ . '/api/post.php';
 require __DIR__ . '/api/forum.php';
-require __DIR__ . '/api/friendships.php';
-require __DIR__ . '/api/upload.php';
-require __DIR__ . '/api/library.php';
+require __DIR__ . '/api/search.php';
 require __DIR__ . '/api/notifications.php';
 require __DIR__ . '/api/admin.php';
